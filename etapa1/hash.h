a0c767d93d327e1e5d73f5eff72dcfadd47bf659
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2019-03-22 04:52:00 -0300

    Create hash header

---
+++ etapa1/hash.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define HASH_SIZE 30
+
+struct hashNode {
+  int type;
+  char *text;
+  struct hashNode *next;
+};
+
+struct hashNode *Table[HASH_SIZE];
+
+int hashAddress(char *text);
+struct hashNode *hashInsert(int type, char *text);
+void hashPrint(void);
+struct hashNode hashFind(char *text);
