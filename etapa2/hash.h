3e82dfe19a837d22252644c8d3a52449996126ec
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-04-05 00:27:39 -0300

    copy hash from stage1

---
+++ etapa2/hash.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define HASH_SIZE 997
+
+struct hashNode {
+  int type;
+  char *text;
+  struct hashNode *next;
+};
+
+struct hashNode *Table[HASH_SIZE];
+
+int hashAddress(char *text);
+struct hashNode* hashInsert(int type, char *text);
+struct hashNode* hashFind(char *text);
+void hashPrint(void);
