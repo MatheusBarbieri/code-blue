42d867379f044427a8d606806b94eb2ab29214b7
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2019-04-06 18:50:36 -0300

    Return exit 0 if file is correct parsed

--- etapa2/main.c
+++ etapa2/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "parser.tab.h"
+
+extern FILE *yyin;
+void initMe(void);
 
 int main (int argc, char **argv)
 {
-  return yyparse();
+  	if (argc < 2) {
+  		fprintf(stderr, "Call: a.out nome_do_arquivo\n");
+  		exit(1);
+  	}
+
+  	if ((yyin = fopen(argv[1], "r")) == 0){
+  		fprintf(stderr, "Nao foi possivel abrir o arquivo \%s\"\n", argv[1]);
+  		exit(2);
+  	}
+
+  	initMe();
+
+  	int result = yyparse();
+
+
+  	if(result == 0){
+          fprintf(stderr, "Arquivo aceito pela gramatica!\n");
+          exit(0);
+      }
 }
