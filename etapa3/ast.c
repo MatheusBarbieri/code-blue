18da495695060ed0774c0e2dc73b5cf26c221ac3
MatheusBarbieri <mcb_tk@hotmail.com>
2019-05-01 23:00:14 +0000

    Remove unused main

--- etapa3/ast.c
+++ etapa3/ast.c
@@ -87,29 +87,3 @@ void astPrint(int level, AST *node){
 	for (i=0; i<MAX_NODES; i++)
 		astPrint(level+1, node->son[i]);
 }
-
-/* int main() {
-
-struct hashNode *node1 = hashInsert(1, "cristiano");
-struct hashNode *node2 = hashInsert(1, "salla");
-struct hashNode *node3 = hashInsert(1, "lalala");
-struct hashNode *node4 = hashInsert(1, "lunardi");
-struct hashNode *node5 = hashInsert(1, "teste");
-struct hashNode *node6 = hashInsert(1, "stringa");
-struct hashNode *node7 = hashInsert(1, "inteiro");
-//hashPrint();
-
-
-
-AST *tree7 = astCreate(1, node7, 0, 0 ,0, 0);
-AST *tree6 = astCreate(1, node6, 0, 0 ,0, 0);
-AST *tree5 = astCreate(1, node5, 0, 0 ,0, 0);
-AST *tree4 = astCreate(1, node4, 0, 0,0, 0);
-AST *tree3 = astCreate(1, node3, tree6, tree7 ,0, 0);
-AST *tree2 = astCreate(1, node2, tree4, tree5 ,0, 0);
-AST *tree1 = astCreate(1, node1, tree2, tree3, 0, 0);
-
-astPrint(0, tree1);
-//printf("(Table[0] = %s)\n", Table[97]->text);
-return -1;
-} */
