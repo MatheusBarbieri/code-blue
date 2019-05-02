f6d47bbadc705418d2e6374312a19b9565f1c728
MatheusBarbieri <mcb_tk@hotmail.com>
2019-05-02 03:40:39 +0000

    Update main to work with stage 3

--- etapa3/main.c
+++ etapa3/main.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parser.tab.h"
+#include "ast.h"
 
 extern FILE *yyin;
 void initMe(void);
+AST* getAST();
+void astToFile();
 
 int main (int argc, char **argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "Error 1. Must call: a.out file_name\n");
+	FILE *outputFile;
+
+	if (argc < 3) {
+		fprintf(stderr, "Error 1. Must call: ./etapa3 inputfile outputfile\n");
 		exit(1);
 	}
 
 	if ((yyin = fopen(argv[1], "r")) == 0){
-		fprintf(stderr, "Error 2. Cannot open file. \%s\"\n", argv[1]);
+		fprintf(stderr, "Error 2. Cannot open file. %s\n", argv[1]);
 		exit(2);
 	}
 
-	initMe();
+	if ((outputFile = fopen(argv[2], "w")) == 0) {
+		fprintf(stderr, "Error 3. Cannot create file %s\n", argv[2]);
+		exit(3);
+	}
 
 	int result = yyparse();
 
+	AST* ast = getAST();
+	astToFile(ast, outputFile);
 
 	if(result == 0){
-		fprintf(stderr, "Success!\n");
 		exit(0);
 	}
 }
