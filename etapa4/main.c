e21f017ecac9261a692369cc77d7ff7d2a62a680
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-05-31 17:52:36 -0300

    Update main file

--- etapa4/main.c
+++ etapa4/main.c
@@ -3,21 +3,18 @@
 #include "parser.tab.h"
 
 extern FILE *yyin;
-void initMe(void);
 
 int main (int argc, char **argv)
 {
 	if (argc < 2) {
-		fprintf(stderr, "Error 1. Must call: a.out file_name\n");
+		fprintf(stderr, "[Error] Missing file path. Must call: ./etapa4 <input_file_path>\n");
 		exit(1);
 	}
 
 	if ((yyin = fopen(argv[1], "r")) == 0){
-		fprintf(stderr, "Error 2. Cannot open file. \%s\"\n", argv[1]);
+		fprintf(stderr, "[Error] Cannot open file: %s\n", argv[1]);
 		exit(2);
 	}
-
-	initMe();
 
 	int result = yyparse();
 
